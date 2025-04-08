172e483d2903b263bcacfb5c1563d981aa8de224
lausan3 <lauanthony4455@gmail.com>
2025-04-08 00:13:52 -0400

    Add variable color scheme to Accent Color asset and add it to TabBarItem's blur

--- Musafirly/Musafirly/Components/Tab Bar/CustomTabItem.swift
+++ Musafirly/Musafirly/Components/Tab Bar/CustomTabItem.swift
@@ -11,14 +11,18 @@ struct CustomTabItem: View {
     let tab: Tab
     let isSelected: Bool
     
+    @Environment(\.colorScheme) private var colorScheme
+    
     var body: some View {
+        let selectedColor: Color = colorScheme == .dark ? .white : .black
+        
         VStack(spacing: 4) {
             Image(systemName: isSelected ? "\(tab.icon).fill" : tab.icon)
                 .font(.system(size: 20))
             Text(tab.title)
                 .font(.caption)
         }
-        .foregroundStyle(isSelected ? Color.white : Color.gray.opacity(0.7))
+        .foregroundStyle(isSelected ? selectedColor : Color.gray.opacity(0.7))
         .frame(maxWidth: .infinity)
     }
 }
