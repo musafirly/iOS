6a307d793f1d453be8ca3c03a50b5a1f12f598e7
lausan3 <lauanthony4455@gmail.com>
2025-05-13 15:07:38 -0400

    Add LocationManager

---
+++ Musafirly/Musafirly/Features/LocationManager.swift
@@ -0,0 +1,50 @@
+//
+//  LocationManager.swift
+//  Musafirly
+//
+//  Created by Anthony on 5/13/25.
+//
+
+
+import Foundation
+import CoreLocation
+
+final class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    @Published var lastKnownLocation: CLLocationCoordinate2D?
+    var manager = CLLocationManager()
+    
+    
+    func checkLocationAuthorization() {
+        
+        manager.delegate = self
+        manager.startUpdatingLocation()
+        
+        
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            //The user cannot change this app’s status, possibly due to active restrictions such as parental controls being in place.
+        case .restricted:
+            print("Location restricted")
+        case .denied:
+            print("Location denied")
+        case .authorizedAlways:
+            print("Location authorizedAlways")
+        case .authorizedWhenInUse:
+            print("Location authorized when in use")
+            lastKnownLocation = manager.location?.coordinate
+            
+        @unknown default:
+            print("Location service disabled")
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
